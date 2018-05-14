972ce330f34ee20e3d70a86dc836166cc49e98c0
BunnyGray <916015935@qq.com>
2018-05-14 16:14:49 +0800

    format code

--- src/main/java/cn/tarena/ht/controller/LoginController.java
+++ src/main/java/cn/tarena/ht/controller/LoginController.java
@@ -32,15 +32,14 @@ public class LoginController extends BaseController {
 
     @RequestMapping("login")
     public String login(String username, String password, String usertype, String valicode, Model model, HttpSession session) {
-
         if (valicode == null) {
             model.addAttribute("errorInfo", "请输入验证码！");
             return "/sysadmin/login/login";
         }
         String code = session.getAttribute("valicode") == null ? "" : (String) session.getAttribute("valicode");
         if (!code.equalsIgnoreCase(valicode)) {
-//            model.addAttribute("errorInfo", "验证码错误！");
-//            return "/sysadmin/login/login";
+            //            model.addAttribute("errorInfo", "验证码错误！");
+            //            return "/sysadmin/login/login";
         }
         if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
             model.addAttribute("errorInfo", "用户名或密码不能为空！");
@@ -62,7 +61,6 @@ public class LoginController extends BaseController {
             model.addAttribute("errorInfo", "用户名或密码不正确！");
             return "/sysadmin/login/login";
         }
-
         //把登录成功的用户信息 保存到session域中
         session.setAttribute("_CURRENT_USER", user);
         //登录成功跳转到首页
